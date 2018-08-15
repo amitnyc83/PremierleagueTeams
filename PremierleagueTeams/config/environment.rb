7ecc788a60929a5ca93e8b2a08ac1a62e7ded059
Amit Singh <amit_nyc@live.com>
2018-08-15 17:06:36 +0000

    adding files

---
+++ PremierleagueTeams/config/environment.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+
+require_relative '../lib/PremierleagueTeams/cli'
+require_relative '../lib/PremierleagueTeams/player'
+require_relative '../lib/PremierleagueTeams/scraper'
+require_relative '../lib/PremierleagueTeams/version'
---
+++ PremierleagueTeams/lib/PremierleagueTeams/cli.rb
@@ -0,0 +1,18 @@
+class PremierleagueTeams::CLI
+
+  def call
+   list_teams
+   list_players
+   list_player_stats
+   more_info_about_player
+  end
+
+
+  def list_teams
+
+  end
+
+
+
+
+end
